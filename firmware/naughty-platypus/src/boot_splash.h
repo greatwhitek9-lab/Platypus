0c59ad439e05da3744559c77783bd6800c9a336e
UrbanPoacher <greatwhitek9@gmail.com>
2026-07-10 06:06:19 +0000

    Add optional Naughty Platypus boot splash interface

---
+++ firmware/naughty-platypus/src/boot_splash.h
@@ -0,0 +1,9 @@
+#ifndef NAUGHTY_PLATYPUS_BOOT_SPLASH_H
+#define NAUGHTY_PLATYPUS_BOOT_SPLASH_H
+
+#include <stdbool.h>
+
+int np_boot_splash_show(void);
+bool np_boot_splash_available(void);
+
+#endif
